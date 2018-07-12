3d050b1c68057ec608b4b7cad71301b5af754ed6
chenkuifang <314287696@qq.com>
2018-07-12 17:46:02 +0800

    add

--- src/main/resources/manual_schema.sql
+++ src/main/resources/manual_schema.sql
@@ -48,3 +48,64 @@ CREATE SCHEMA IF NOT EXISTS demo_ds_slave_1;
 -- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_0.t_order_item_1 (order_item_id BIGINT NOT NULL AUTO_INCREMENT, order_id BIGINT NOT NULL, user_id INT NOT NULL, PRIMARY KEY (order_item_id));
 -- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_order_item_0 (order_item_id BIGINT NOT NULL AUTO_INCREMENT, order_id BIGINT NOT NULL, user_id INT NOT NULL, PRIMARY KEY (order_item_id));
 -- CREATE TABLE IF NOT EXISTS demo_ds_master_1_slave_1.t_order_item_1 (order_item_id BIGINT NOT NULL AUTO_INCREMENT, order_id BIGINT NOT NULL, user_id INT NOT NULL, PRIMARY KEY (order_item_id));
+-- ----------------------------
+-- Table structure for t_order_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_0`;
+CREATE TABLE `t_order_0` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单编码',
+  `goods_id` bigint(40) NOT NULL COMMENT '商品ID',
+  `goods_name` varchar(80) DEFAULT NULL COMMENT '商品名称',
+  `price` decimal(20,2) DEFAULT '0.00' COMMENT '拍下价格',
+  `user_id` bigint(20) DEFAULT NULL,
+  `amount` bigint(20) DEFAULT '0' COMMENT '商品数量',
+  `buyer_name` varchar(80) DEFAULT NULL COMMENT '收货人',
+  `buyer_phone` varchar(80) DEFAULT NULL COMMENT '手机',
+  `status` int(11) DEFAULT '0' COMMENT '订单状态 0:未付款，1：已付款，2：已发货，3：已完成，4：取消订单，5：支付失败',
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=224215976852324353 DEFAULT CHARSET=utf8 COMMENT='订单信息表';
+
+-- ----------------------------
+-- Table structure for t_order_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_1`;
+CREATE TABLE `t_order_1` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单编码',
+  `goods_id` bigint(40) NOT NULL COMMENT '商品ID',
+  `goods_name` varchar(80) DEFAULT NULL COMMENT '商品名称',
+  `price` decimal(20,2) DEFAULT '0.00' COMMENT '拍下价格',
+  `user_id` bigint(20) DEFAULT NULL,
+  `amount` bigint(20) DEFAULT '0' COMMENT '商品数量',
+  `buyer_name` varchar(80) DEFAULT NULL COMMENT '收货人',
+  `buyer_phone` varchar(80) DEFAULT NULL COMMENT '手机',
+  `status` int(11) DEFAULT '0' COMMENT '订单状态 0:未付款，1：已付款，2：已发货，3：已完成，4：取消订单，5：支付失败',
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=224212254973755393 DEFAULT CHARSET=utf8 COMMENT='订单信息表';
+
+-- ----------------------------
+-- Table structure for t_user0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user0`;
+CREATE TABLE `t_user0` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) DEFAULT NULL COMMENT '登陆用户名',
+  `password` varchar(255) DEFAULT NULL COMMENT '登陆密码',
+  `mobile` varchar(11) DEFAULT NULL COMMENT '手机',
+  `sex` varchar(2) DEFAULT '1' COMMENT '性别',
+  `status` int(11) DEFAULT '1' COMMENT '状态 1 正常, 0 停用',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+-- ----------------------------
+-- Table structure for t_user1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user1`;
+CREATE TABLE `t_user1` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) DEFAULT NULL COMMENT '登陆用户名',
+  `password` varchar(255) DEFAULT NULL COMMENT '登陆密码',
+  `mobile` varchar(11) DEFAULT NULL COMMENT '手机',
+  `sex` varchar(2) DEFAULT '1' COMMENT '性别',
+  `status` int(11) DEFAULT '1' COMMENT '状态 1 正常, 0 停用',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
